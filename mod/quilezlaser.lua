1268137aac537ba799d061a1021d29e18538c645
Sir Code <taylorschneider07@yahoo.com>
2022-03-16 16:01:29 -0400

    Update quilezlaser.lua

--- mod/quilezlaser.lua
+++ mod/quilezlaser.lua
@@ -20,6 +20,7 @@ local tool = {
     ammo = 10000
 }
 local maxDist = 1000
+local qlDeflectors = {}
 
 function updateLaserMode()
 	SetInt("savegame.mod.laserMode", GetInt("savegame.mod.laserMode") + 1)
@@ -28,53 +29,53 @@ function updateLaserMode()
 	end
 end
 
-function shootLaser(body, shape, origin, dir, currentLength, deflectorsHit)
-	QueryRequire("physical")
-	if shape ~= 0 then
-		QueryRejectShape(shape)
-	end
-	if body ~= 0 then
-		QueryRejectBody(body)
-	end
-	local hit, hitDist, hitNormal, hitShape = QueryRaycast(origin, dir, maxDist, 0.0, true)
-	local length = maxDist
-	if hit then
-		length = hitDist
-	end
-	local hitPoint = VecAdd(origin, VecScale(dir, length))
-	local t = Transform(VecLerp(origin, hitPoint, 0.5))
-	local xAxis = VecNormalize(VecSub(hitPoint, origin))
-	local zAxis = VecNormalize(VecSub(origin, GetCameraTransform().pos))
-	t.rot = QuatAlignXZ(xAxis, zAxis)
-	DrawSprite(laserSpriteOg, t, length, 0.05 + math.random() * 0.01, 8, 4, 4, 1, true, true)
-	local col = laserColors[GetInt("savegame.mod.laserMode")]
-	DrawSprite(laserSpriteOg, t, length, 0.5, col[1], col[2], col[3], 1, true, true)
-	local hitBody = GetShapeBody(hitShape)
-	if HasTag(hitBody, "mirror2") and currentLength < maxDist then
-		local alreadyHit = false
-		for i = 1, #deflectorsHit do
-			if deflectorsHit[i] == hitBody then
-				alreadyHit = true
-				break
-			end
-		end
-		if not alreadyHit then
-			deflectorsHit[#deflectorsHit+1] = hitBody
-			local t = GetBodyTransform(hitBody)
-			local refDir = TransformToParentVec(t, Vec(0, 0, 1))
-			SetShapeEmissiveScale(hitShape, 1)
-			return shootLaser(hitBody, hitShape, t.pos, refDir, currentLength + length, deflectorsHit)
-		end
-	end
-	local hitBody = GetShapeBody(hitShape)
-	if HasTag(hitBody, "mirror") and currentLength < maxDist then
-		local refDir = VecSub(dir, VecScale(hitNormal, VecDot(hitNormal, dir) * 2))
-		return shootLaser(hitBody, hitShape, hitPoint, refDir, currentLength + length, deflectorsHit)
-	end
-	return currentLength + length, hitPoint, hitBody, hitShape
-end
+-- function shootLaser(body, shape, origin, dir, currentLength, deflectorsHit)
+-- 	QueryRequire("physical")
+-- 	if shape ~= 0 then
+-- 		QueryRejectShape(shape)
+-- 	end
+-- 	if body ~= 0 then
+-- 		QueryRejectBody(body)
+-- 	end
+-- 	local hit, hitDist, hitNormal, hitShape = QueryRaycast(origin, dir, maxDist, 0.0, true)
+-- 	local length = maxDist
+-- 	if hit then
+-- 		length = hitDist
+-- 	end
+-- 	local hitPoint = VecAdd(origin, VecScale(dir, length))
+-- 	local t = Transform(VecLerp(origin, hitPoint, 0.5))
+-- 	local xAxis = VecNormalize(VecSub(hitPoint, origin))
+-- 	local zAxis = VecNormalize(VecSub(origin, GetCameraTransform().pos))
+-- 	t.rot = QuatAlignXZ(xAxis, zAxis)
+-- 	DrawSprite(laserSpriteOg, t, length, 0.05 + math.random() * 0.01, 8, 4, 4, 1, true, true)
+-- 	local col = laserColors[GetInt("savegame.mod.laserMode")]
+-- 	DrawSprite(laserSpriteOg, t, length, 0.5, col[1], col[2], col[3], 1, true, true)
+-- 	local hitBody = GetShapeBody(hitShape)
+-- 	if HasTag(hitBody, "mirror2") and currentLength < maxDist then
+-- 		local alreadyHit = false
+-- 		for i = 1, #deflectorsHit do
+-- 			if deflectorsHit[i] == hitBody then
+-- 				alreadyHit = true
+-- 				break
+-- 			end
+-- 		end
+-- 		if not alreadyHit then
+-- 			deflectorsHit[#deflectorsHit+1] = hitBody
+-- 			local t = GetBodyTransform(hitBody)
+-- 			local refDir = TransformToParentVec(t, Vec(0, 0, 1))
+-- 			SetShapeEmissiveScale(hitShape, 1)
+-- 			return shootLaser(hitBody, hitShape, t.pos, refDir, currentLength + length, deflectorsHit)
+-- 		end
+-- 	end
+-- 	local hitBody = GetShapeBody(hitShape)
+-- 	if HasTag(hitBody, "mirror") and currentLength < maxDist then
+-- 		local refDir = VecSub(dir, VecScale(hitNormal, VecDot(hitNormal, dir) * 2))
+-- 		return shootLaser(hitBody, hitShape, hitPoint, refDir, currentLength + length, deflectorsHit)
+-- 	end
+-- 	return currentLength + length, hitPoint, hitBody, hitShape
+-- end
 
-function drawlaser(pos1, pos2, color, size)
+function drawlaserSprite(pos1, pos2, color, size)
     visual.drawline(laserSprite, pos1, pos2, {
         r = color[1], 
         g = color[2], 
@@ -82,6 +83,13 @@ function drawlaser(pos1, pos2, color, size)
         additive = true,
         width = size
     })
+end
+
+function drawLaser(startpos, endpos, col, brt)
+	PointLight(endpos, col[1], col[2], col[3], 1)
+	PointLight(endpos, brt[1], brt[2], brt[3], 0.01)
+	drawlaserSprite(startpos, endpos, col, 0.5)
+	drawlaserSprite(startpos, endpos, brt, 0.05)
 end
 
 function tool:Initialize()
@@ -116,6 +124,40 @@ function tool:GetTarget()
 end
 
 function tool:LeftClick()
+    local target = PLAYER:GetCamera():Raycast(maxDist, -1)
+	local mode = GetInt("savegame.mod.laserMode")
+	local col = laserColors[GetInt("savegame.mod.laserMode")]
+	local brt = brightness[GetInt("savegame.mod.laserMode")]
+	local length = 0;
+	local newCol = {};
+	local dir = VecNormalize(VecSub(target.hitpos, self:GetBoneGlobalTransform('nozzle').pos))
+	local hitBody = GetEntityHandle(target.shape:GetBody())
+	-- Draw Laser
+
+		drawLaser(self:GetBoneGlobalTransform('nozzle').pos, target.hitpos, col, brt)
+		
+	-- Do other stuff
+	if (target.shape:GetBody()):HasTag('mirror2') then
+	elseif (target.shape:GetBody()):HasTag('mirror') then
+	else
+	end
+	if mode == 1 then
+		MakeHole(target.hitpos, 0.5, 0.3, 0.1, true)
+		SpawnFire(target.hitpos)
+	elseif mode == 2 then
+		local curPos = target.hitpos
+		for i=1, 5 do
+			curPos = VecAdd(curPos, VecScale(VecScale(dir, dt), 6))
+			MakeHole(curPos, 0.5, 0.4, 0.3, true)
+		end
+	else
+		local curPos = target.hitpos
+		for i=1, 5 do
+			curPos = VecAdd(curPos, VecScale(VecScale(dir, dt), 6))
+			SpawnFire(curPos)
+			emitSmoke(curPos, 1.0)
+		end
+	end
 end
 
 function tool:RightClick()
@@ -128,63 +170,8 @@ function tool:RightClickReleased()
 end
 
 function tool:Tick(dt)
-	QueryRequire("physical")
-    SetToolTransform(toolpos)
-    local target = PLAYER:GetCamera():Raycast(maxDist, -1)
-	local mode = GetInt("savegame.mod.laserMode")
-    -- local target = self:GetBoneGlobalTransform('root'):Raycast(maxDist, -1)
     if InputPressed("alt") then
         updateLaserMode()
-    end
-    if InputDown('lmb') then
-        local col = laserColors[GetInt("savegame.mod.laserMode")]
-        local brt = brightness[GetInt("savegame.mod.laserMode")]
-		local length = 0;
-        local newCol = {};
-        PointLight(target.hitpos, col[1], col[2], col[3], 1)
-        PointLight(target.hitpos, brt[1], brt[2], brt[3], 0.01)
-        PointLight(self:GetBoneGlobalTransform('tip').pos, col[1], col[2], col[3], 1)
-        drawlaser(self:GetBoneGlobalTransform('nozzle').pos, target.hitpos, col, 0.5)
-        drawlaser(self:GetBoneGlobalTransform('nozzle').pos, target.hitpos, brt, 0.05)
-        local dir = VecNormalize(VecSub(target.hitpos, self:GetBoneGlobalTransform('nozzle').pos))
-		local hitBody = GetEntityHandle(target.shape:GetBody())
-        if (target.shape:GetBody()):HasTag('mirror2') then
-			local alreadyHit = false
-			for i = 1, #deflectorsHit do
-				if deflectorsHit[i] == hitBody then
-					alreadyHit = true
-					break
-				end
-			end
-			if not alreadyHit then
-				deflectorsHit[#deflectorsHit+1] = hitBody
-				local t = GetBodyTransform(hitBody)
-				local refDir = TransformToParentVec(t, Vec(0, 0, 1))
-				SetShapeEmissiveScale(hitShape, 1)
-				local length, hitPoint, hitBody, hitShape = shootLaser(hitBody, hitShape, t.pos, refDir, currentLength + length, deflectorsHit)
-			end
-		elseif (target.shape:GetBody()):HasTag('mirror') then
-			local refDir = VecSub(dir, VecScale(hitNormal, VecDot(hitNormal, dir) * 2))
-			return shootLaser(GetEntityHandle(target.shape:GetBody()), hitShape, hitPoint, refDir, currentLength + length, deflectorsHit)
-        else
-        end
-		if mode == 1 then
-			MakeHole(target.hitpos, 0.5, 0.3, 0.1, true)
-			SpawnFire(target.hitpos)
-		elseif mode == 2 then
-			local curPos = target.hitpos
-			for i=1, 5 do
-				curPos = VecAdd(curPos, VecScale(VecScale(dir, dt), 6))
-				MakeHole(curPos, 0.5, 0.4, 0.3, true)
-			end
-		else
-			local curPos = target.hitpos
-			for i=1, 5 do
-				curPos = VecAdd(curPos, VecScale(VecScale(dir, dt), 6))
-				SpawnFire(curPos)
-				emitSmoke(curPos, 1.0)
-			end
-		end
     end
 end
 
